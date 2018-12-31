9ca1a972f4c57768712ec254f4d877532c514b8c
Cristopher Castro <cristophernic@live.cl>
2018-12-30 23:31:05 -0300

    .

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -9,6 +9,11 @@
     <link rel="icon" href="data:;base64,=">
     <!-- CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <style>
+        body {
+            background-image: url("<?php echo Config::get('URL'); ?>img/texture.png");
+        }
+    </style>
 </head>
 <body>
     <!-- navbar -->
--- application/view/dashboard/index.php
+++ application/view/dashboard/index.php
@@ -12,29 +12,38 @@
     </nav>
     <div class="tab-content" id="nav-tabContent">
         <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-            <div class="card">
+            <div class="card mt-2">
                 <div class="card-body">
                     <h5 class="card-title">Pacientes</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
         </div>
         <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-            <div class="card">
+            <div class="card mt-2">
                 <div class="card-body">
                     <h5 class="card-title">Horas de atención</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <div class="row">
+                        <div class="col-8">
+                        </div>
+                        <div class="col-4">
+                            <div class="btn-group" role="group">
+                                <button type="button" class="btn btn-primary">Nuevo</button>
+                                <button type="button" class="btn btn-secondary">Modificar</button>
+                                <button type="button" class="btn btn-secondary">Guardar</button>
+                                <button type="button" class="btn btn-secondary">Cancelar</button>
+                                <button type="button" class="btn btn-secondary">Eliminar</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
-            <div class="card">
+            <div class="card mt-2">
                 <div class="card-body">
                     <h5 class="card-title">Resumen</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
         </div>
