83aa0123b22a106eea16ec611928638fb256148a
Cristopher Castro <cristophernic@live.cl>
2018-12-30 23:41:48 -0300

    .

--- application/view/dashboard/index.php
+++ application/view/dashboard/index.php
@@ -12,61 +12,61 @@
     </nav>
     <div class="tab-content" id="nav-tabContent">
         <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-            <div class="card mt-2">
-                <div class="card-body">
-                    <h5 class="card-title">Pacientes</h5>
-                    <div class="row">
-                        <div class="col-8">
+            <div class="row">
+                <div class="col-9">
+                    <div class="card mt-2">
+                        <div class="card-body">
+                            <h5 class="card-title">Pacientes</h5>
                         </div>
-                        <div class="col-4">
-                            <div class="btn-group-vertical" role="group">
-                                <button type="button" class="btn btn-primary">Nuevo</button>
-                                <button type="button" class="btn btn-secondary">Modificar</button>
-                                <button type="button" class="btn btn-secondary">Guardar</button>
-                                <button type="button" class="btn btn-secondary">Cancelar</button>
-                                <button type="button" class="btn btn-secondary">Eliminar</button>
-                            </div>
-                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="btn-group-vertical" role="group">
+                        <button type="button" class="btn btn-primary">Nuevo</button>
+                        <button type="button" class="btn btn-secondary">Modificar</button>
+                        <button type="button" class="btn btn-secondary">Guardar</button>
+                        <button type="button" class="btn btn-secondary">Cancelar</button>
+                        <button type="button" class="btn btn-secondary">Eliminar</button>
                     </div>
                 </div>
             </div>
         </div>
         <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-            <div class="card mt-2">
-                <div class="card-body">
-                    <h5 class="card-title">Horas de atención</h5>
-                    <div class="row">
-                        <div class="col-8">
+            <div class="row pt-2">
+                <div class="col-9">
+                    <div class="card mt-2">
+                        <div class="card-body">
+                            <h5 class="card-title">Horas de atención</h5>
                         </div>
-                        <div class="col-4">
-                            <div class="btn-group-vertical" role="group">
-                                <button type="button" class="btn btn-primary">Nuevo</button>
-                                <button type="button" class="btn btn-secondary">Modificar</button>
-                                <button type="button" class="btn btn-secondary">Guardar</button>
-                                <button type="button" class="btn btn-secondary">Cancelar</button>
-                                <button type="button" class="btn btn-secondary">Eliminar</button>
-                            </div>
-                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="btn-group-vertical" role="group">
+                        <button type="button" class="btn btn-primary">Nuevo</button>
+                        <button type="button" class="btn btn-secondary">Modificar</button>
+                        <button type="button" class="btn btn-secondary">Guardar</button>
+                        <button type="button" class="btn btn-secondary">Cancelar</button>
+                        <button type="button" class="btn btn-secondary">Eliminar</button>
                     </div>
                 </div>
             </div>
         </div>
         <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
-            <div class="card mt-2">
-                <div class="card-body">
-                    <h5 class="card-title">Resumen</h5>
-                    <div class="row">
-                        <div class="col-8">
+            <div class="row pt-2">
+                <div class="col-9">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Resumen</h5>
                         </div>
-                        <div class="col-4">
-                            <div class="btn-group-vertical" role="group">
-                                <button type="button" class="btn btn-primary">Nuevo</button>
-                                <button type="button" class="btn btn-secondary">Modificar</button>
-                                <button type="button" class="btn btn-secondary">Guardar</button>
-                                <button type="button" class="btn btn-secondary">Cancelar</button>
-                                <button type="button" class="btn btn-secondary">Eliminar</button>
-                            </div>
-                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="btn-group-vertical" role="group">
+                        <button type="button" class="btn btn-primary">Nuevo</button>
+                        <button type="button" class="btn btn-secondary">Modificar</button>
+                        <button type="button" class="btn btn-secondary">Guardar</button>
+                        <button type="button" class="btn btn-secondary">Cancelar</button>
+                        <button type="button" class="btn btn-secondary">Eliminar</button>
                     </div>
                 </div>
             </div>
