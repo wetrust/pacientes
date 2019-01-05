bfa1ab224775c0e31d409f3416a8bd595772a36d
Cristopher Castro <cristophernic@live.cl>
2019-01-05 13:24:30 -0300

    .

--- application/view/dashboard/index.php
+++ application/view/dashboard/index.php
@@ -46,6 +46,7 @@
                     </ul>
                     <div class="card">
                         <div class="card-body">
+                            <h5 class="card-title">Datos iniciales</h5>
                             <div class="form-row">
                                 <div class="form-group col-md-4">
                                     <label for="inputEmail4">RUT</label>
@@ -135,35 +136,9 @@
                                     </div>
                                 </div>
                             </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="card mt-2">
-                                <div class="card-body">
-                                    <table class="table table-striped table-hover">
-                                        <thead>
-                                            <tr>
-                                            <th scope="col">#</th>
-                                            <th scope="col">RUT</th>
-                                            <th scope="col">Nombre</th>
-                                            <th scope="col">Apellidos</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody id="pacientes.tabla">
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="tab-pane fade" id="nav-clinico" role="tabpanel" aria-labelledby="nav-clinico-tab">
-                    <h1>Horas de atención</h1>
-                    <hr>
-                    <div class="row pt-2">
-                        <div class="col-10">
                             <div class="card">
                                 <div class="card-body">
-                                    <h5 class="card-title">Datos Clinicos</h5>
+                                    <h5 class="card-title">Datos clinicos</h5>
                                     <div class="form-row">
                                         <div class="form-group col-md-4">
                                         <label for="inputEmail4">Fecha de nacimiento</label>
@@ -227,14 +202,23 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-2">
-                            <div class="btn-group-vertical" role="group">
-                                <button type="button" class="btn btn-primary">Nuevo</button>
-                                <button type="button" class="btn btn-secondary d-none">Modificar</button>
-                                <button type="button" class="btn btn-secondary d-none">Guardar</button>
-                                <button type="button" class="btn btn-secondary d-none">Cancelar</button>
-                                <button type="button" class="btn btn-secondary d-none">Eliminar</button>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card mt-2">
+                                <div class="card-body">
+                                    <table class="table table-striped table-hover">
+                                        <thead>
+                                            <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">RUT</th>
+                                            <th scope="col">Nombre</th>
+                                            <th scope="col">Apellidos</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody id="pacientes.tabla">
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
