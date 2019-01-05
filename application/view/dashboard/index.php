e238f1e3c0e36936bbcd759cf2075011fe475290
Cristopher Castro <cristophernic@live.cl>
2019-01-05 13:26:12 -0300

    .

--- application/view/dashboard/index.php
+++ application/view/dashboard/index.php
@@ -136,7 +136,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="card">
+                            <div class="card my-2">
                                 <div class="card-body">
                                     <h5 class="card-title">Datos clinicos</h5>
                                     <div class="form-row">
@@ -202,9 +202,7 @@
                                     </div>
                                 </div>
                             </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="card mt-2">
+                            <div class="card">
                                 <div class="card-body">
                                     <table class="table table-striped table-hover">
                                         <thead>
@@ -220,8 +218,6 @@
                                     </table>
                                 </div>
                             </div>
-                        </div>
-                    </div>
                 </div>
                 <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
                     <h1>Horas de atención</h1>
